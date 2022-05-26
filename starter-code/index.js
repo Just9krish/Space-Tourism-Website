445b8749faae1083c1e7f8d0d75f1c5d581c2577
just9krish <just9krish434@gamil.com>
2022-05-26 21:55:53 +0530

    progressing with navigation

---
+++ starter-code/index.js
@@ -0,0 +1,14 @@
+const mobileNavToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNavigation = document.querySelector(".primary-navigation");
+
+mobileNavToggle.addEventListener("click", () => {
+  const visibility = primaryNavigation.getAttribute("data-is-visible");
+
+  if (visibility === "false") {
+    primaryNavigation.setAttribute("data-is-visible", true);
+    mobileNavToggle.setAttribute("aria-expanded", true);
+  } else {
+    primaryNavigation.setAttribute("data-is-visible", false);
+    mobileNavToggle.setAttribute("aria-expanded", false);
+  }
+});
